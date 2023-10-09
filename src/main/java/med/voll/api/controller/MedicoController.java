4988406baa9824dbe272175199dbce3e37d8ce2f
tamiris-rtl <146030970+tamiris-rtl@users.noreply.github.com>
2023-10-09 19:03:28 -0300

    Update

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("medicos")
+
 public class MedicoController {
 
     @Autowired
--- src/main/java/med/voll/api/paciente/DadosCadastroPaciente.java
+++ src/main/java/med/voll/api/paciente/DadosCadastroPaciente.java
@@ -1,10 +1,10 @@
 package med.voll.api.paciente;
 
 import jakarta.validation.Valid;
+
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import med.voll.api.endereco.DadosEndereco;
-import med.voll.api.endereco.Endereco;
 
 public record DadosCadastroPaciente(
         @NotBlank
